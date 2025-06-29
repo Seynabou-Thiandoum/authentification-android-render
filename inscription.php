7ad48dc7d56eff6188e0471b280fd8730ac81f56
ZeynabThiandoum <thiandoumzeynab8@gmail.com>
2025-06-29 15:02:06 +0000

    Rectification de registre.php

--- inscription.php
+++ inscription.php
@@ -14,8 +14,8 @@ $dbPort = $dbParts['port'] ?? '5432'; // si "port" existe, on l’utilise, sinon
 $dbUser = $dbParts['user'];
 $dbPass = $dbParts['pass'];
 $dbName = ltrim($dbParts['path'], '/');
-<!-- var_dump($dbUrl);
-var_dump($dbParts); -->
+
+
 try {
     $pdo = new PDO(
         "pgsql:host=$dbHost;port=$dbPort;dbname=$dbName",
@@ -76,41 +76,3 @@ try {
     }
 }
 ?>
-
-
-
-
-
-
-
-<!-- <?php
-
-header("Content-type: application/json");
-
-$mysqli = new mysqli("sql210.infinityfree.com","if0_39155606","2002banyeZ","if0_39155606_XXX");
-
-// Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
-//echo "connexion est ok" ;
-
- $reponses="";
-
-if(!empty($_POST["nom"]) && !empty($_POST["prenom"])&&  !empty($_POST["username"])&&  !empty($_POST["password"])) {
-    // requete sql
-
-    $sql = "insert into users(nom ,prenom , username, password) values ('".$_POST["nom"]."','".$_POST["prenom"]."','".$_POST["username"]."','".$_POST["password"]."')";
-    
-   //  echo "request sql=  ".$sql ;
-    $result = $mysqli -> query($sql);  
-     
-        $reponses= "{'message': 'inscription ok '}";
-}else{
-    $reponses="merci de fournir un user_id " ;
-}
-echo $reponses ;
-
-
-?> -->
